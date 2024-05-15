f6f0847fd88220d2b63381c3cb83b644c9163470
ngoctu <nguyentu85242@gmail.com>
2024-05-15 16:11:05 +0700

    commit second

--- server.js
+++ server.js
@@ -116,6 +116,10 @@ async function getUserTokenByUserName(userId) {
     }
 }
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
