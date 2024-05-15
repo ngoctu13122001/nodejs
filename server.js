558cdc43e605ab730c8b9065a56581e342e3c338
ngoctu <nguyentu85242@gmail.com>
2024-05-15 10:49:32 +0700

    update server

--- server.js
+++ server.js
@@ -5,24 +5,12 @@ const serviceAccount = require('./serviceAccountKey.json');
 const mongoose = require('mongoose');
 
 // Thiết lập kết nối với MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+mongoose.connect('mongodb+srv://ntuuu:WKXMrbadN31Kq34h@nguyenngoctu.i1bpmai.mongodb.net/notify?retryWrites=true&w=majority')
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        "type": "service_account",
-        "project_id": process.env.PROJECT_ID,
-        "private_key_id": process.env.PRIVATE_KEY_ID,
-        "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-        "client_email": process.env.CLIENT_EMAIL,
-        "client_id": process.env.CLIENT_ID,
-        "auth_uri": process.env.AUTH_URI,
-        "token_uri": process.env.TOKEN_URI,
-        "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT_URL,
-        "client_x509_cert_url": process.env.CLIENT_CERT_URL,
-        "universe_domain": process.env.UNIVERSE_DOMAIN
-    }),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://fcm.googleapis.com/v1/projects/qlcv-521e6/messages:send'
 });
 
